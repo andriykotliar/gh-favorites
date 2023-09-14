74bd6b2672017c0bb49ba4c80ee77a925385474f
Andrii Kotliar <andrii.k@radency.com>
2023-09-14 12:23:02 +0300

    initialized apollo client and react router

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { ApolloProvider } from '@apollo/client';
 import App from './App.tsx';
+import { apolloClient } from '@/services';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <ApolloProvider client={apolloClient}>
+        <App />
+      </ApolloProvider>
+    </BrowserRouter>
   </React.StrictMode>,
 );
